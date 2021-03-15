634d6d87c77e5ea2019d501ed18ca2da5754cc86
mikhawa <michaeljpitz@gmail.com>
2021-03-15 15:52:02 +0100

    connectTheUser

--- datas/test-sql.sql
+++ datas/test-sql.sql
@@ -1,4 +1,48 @@
+# getAll() from TheNewsManager  
+SELECT * FROM TheNews ORDER BY theNewsDate DESC
+
+# getAllHomePage() from TheNewsManager
+SELECT n.theNewsTitle, n.theNewsSlug, LEFT(n.theNewsText,180) AS theNewsText, n.theNewsDate, n.theUserIdtheUser,
+                    u.theUserLogin,
+                    GROUP_CONCAT(s.theSectionName SEPARATOR '|||') as theSectionName, GROUP_CONCAT(s.idtheSection) as idtheSection
+                FROM TheNews n 
+                    INNER JOIN TheUser u
+                        ON u.idtheUser = n.theUserIdtheUser
+                    LEFT JOIN theNews_has_theSection h
+                        ON h.theNews_idtheNews = n.idtheNews
+                    LEFT JOIN thesection s 
+                        ON s.idtheSection = h.theSection_idtheSection
+                GROUP BY n.idtheNews
+                ORDER BY n.theNewsDate DESC;
+
+# getAllNewsInTheSection from TheNewsManager
+SELECT n.idtheNews, n.theNewsTitle, n.theNewsSlug, LEFT(n.theNewsText,180) AS theNewsText, n.theNewsDate, n.theUserIdtheUser,
+                    u.theUserLogin,
+                    
+                        (SELECT GROUP_CONCAT(s2.theSectionName ,'|||', s2.idtheSection SEPARATOR '---')
+                            FROM thesection s2
+                            INNER JOIN theNews_has_theSection h2
+                                ON s2.idtheSection = h2.theSection_idtheSection
+                            INNER JOIN TheNews n2    
+                                ON n2.idtheNews = h2.theNews_idtheNews
+                            WHERE   n.idtheNews = n2.idtheNews  
+                        )AS section
+                        
+                FROM TheNews n 
+                    INNER JOIN TheUser u
+                        ON u.idtheUser = n.theUserIdtheUser
+                    LEFT JOIN theNews_has_theSection h
+                        ON h.theNews_idtheNews = n.idtheNews
+                    LEFT JOIN thesection s 
+                        ON s.idtheSection = h.theSection_idtheSection
+                WHERE s.idtheSection = 3        
+                GROUP BY n.idtheNews
+                ORDER BY n.theNewsDate DESC;
+
 SELECT * FROM theuser;
 
-SELECT * FROM theuser
-WHERE theUserLogin='' AND theUserPwd='';
+SELECT u.`idtheUser`,u.`theUserLogin`,u.`theUserMail`,r.theRoleName,r.`theRoleValue`
+    FROM theuser u  
+    INNER JOIN therole r
+        ON u.`theRoleIdtheRole`= r.idtheRole
+    WHERE u.theUserLogin='Mikhawa' AND u.theUserPwd='Mikhawa1717';
--- model/TheUserManager.php
+++ model/TheUserManager.php
@@ -19,7 +19,37 @@ class TheUserManager extends ManagerAbstract implements ManagerInterface {
     
     // connexion à l'administration
     public function connectTheUser(TheUser $user): array{
+        $sql = "SELECT u.`idtheUser`,u.`theUserLogin`,u.`theUserMail`,r.theRoleName,r.`theRoleValue`
+    FROM theuser u  
+    INNER JOIN therole r
+        ON u.`theRoleIdtheRole`= r.idtheRole
+    WHERE u.theUserLogin=? AND u.theUserPwd=?;";
         
+        $prepare = $this->db->prepare($sql);
+        
+        try{
+            // exécution de la requête préparée
+            $prepare->execute([$user->getTheUserLogin(),$user->getTheUserPwd()]);
+            
+            // on a un résultat
+            if($prepare->rowCount()){
+                // récupartion du résultat de la requête
+                $recup = $prepare->fetch(PDO::FETCH_ASSOC);
+                // instanciation avec hydratation
+                $TheUserInstance = new TheUser($recup);
+                // ajout des valeurs externes à la table
+                $TheUserInstance->theRoleName = $recup['theRoleName'];
+                $TheUserInstance->theRoleValue = $recup['theRoleValue'];
+                // la connexion est réussie
+                return [0=>$TheUserInstance];
+                
+            }else{
+                return [1=>"Votre login et/ou mot de passe est incorrecte"];
+            }
+            
+        } catch (PDOException $ex) {
+            return [2=>$ex->getMessage()];
+        }
     }
 
 }
