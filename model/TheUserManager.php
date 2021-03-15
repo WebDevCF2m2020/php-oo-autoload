9d1ea556eb748923e39259a318e0fe76485da9bd
mikhawa <michaeljpitz@gmail.com>
2021-03-15 14:59:37 +0100

    Create TheUserManager.php

---
+++ model/TheUserManager.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TheUserManager
+ *
+ * @author Family PITZ
+ */
+class TheUserManager extends ManagerAbstract implements ManagerInterface {
+    //put your code here
+    public function getAll(): array {
+        
+    }
+    
+    // connexion à l'administration
+    public function connectTheUser(TheUser $user): array{
+        
+    }
+
+}
